79ffaf120cae99316a832a001c7e82c56097ed97
osvaldsoza <osvald.soza@gmail.com>
2022-06-26 12:03:04 -0300

    Finalizando projeto

--- monitoramento.go
+++ monitoramento.go
@@ -4,35 +4,34 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"strconv"
 	"strings"
 	"time"
 )
 
-const monitoramentos = 5
-const delay = 10
+const MONITORAMENTOS = 5
+const DELAY = 10
 
 func main() {
-
 	showTelaBoasVindas()
-	lerSiteDoArquivo()
-	montaMenu()
+	showMenu()
 
 	for {
 		executaOpcao(getOpcao())
-		montaMenu()
+		showMenu()
 	}
-
 }
 
 func executaOpcao(opcao int) {
-
 	switch opcao {
 	case 1:
 		inciandoMnitoramento()
 	case 2:
-		fmt.Println("Exibindo logs")
+		fmt.Println("Exibindo logs...")
+		imprimeLog()
 	case 0:
 		fmt.Println("Saindo do programa.")
 		os.Exit(0)
@@ -42,17 +41,29 @@ func executaOpcao(opcao int) {
 	}
 }
 
+func imprimeLog() {
+	arquivo, err := ioutil.ReadFile("log.txt")
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(arquivo))
+}
+
 func inciandoMnitoramento() {
 
-	sites := lerSiteDoArquivo()
+	sites := lerArquivo("sites.txt")
 
-	for i := 0; i < monitoramentos; i++ {
+	for i := 0; i < MONITORAMENTOS; i++ {
 		fmt.Println("Iniciando...")
 		fmt.Println("Monitoramento", i+1, ":")
+
 		for _, site := range sites {
 			getSite(site)
 		}
-		time.Sleep(delay * time.Second)
+
+		time.Sleep(DELAY * time.Second)
 		fmt.Println("")
 	}
 }
@@ -64,26 +75,33 @@ func getSite(site string) {
 		fmt.Println("Ocorreu erro:", err)
 	}
 
+	var isRunning bool
+
 	if resp.StatusCode == 200 {
 		fmt.Println("Site:", site, "foi carregado com sucesso")
+		isRunning = true
 	} else {
 		fmt.Println("Site:", site, "não encontrado ou com problema. Status code:", resp.StatusCode)
+		isRunning = false
 	}
+	registraLog(site, isRunning)
+
 }
 
-func lerSiteDoArquivo() []string {
+func lerArquivo(arq string) []string {
 	var sites []string
-	arquivo, err := os.Open("sites.txt")
+
+	arquivo, err := os.Open(arq)
 	//arquivo, err := ioutil.ReadFile("sites.txt")
 
 	if err != nil {
-		fmt.Println("Ocorreu erro ao abrir o arquivo:", err)
+		fmt.Println("Ocorreu erro ao tentar abrir o arquivo:", err)
 	}
 
-	info := bufio.NewReader(arquivo)
+	dadosDoArquivo := bufio.NewReader(arquivo)
 
 	for {
-		linhaAtual, err := info.ReadString('\n')
+		linhaAtual, err := dadosDoArquivo.ReadString('\n')
 
 		sites = append(sites, strings.TrimSpace(linhaAtual))
 
@@ -92,7 +110,19 @@ func lerSiteDoArquivo() []string {
 		}
 	}
 
+	arquivo.Close()
 	return sites
+}
+
+func registraLog(site string, status bool) {
+	arquivo, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " - " + site + " - online: " + strconv.FormatBool(status) + "\n")
+	arquivo.Close()
 }
 
 func showTelaBoasVindas() {
@@ -112,7 +142,7 @@ func getOpcao() int {
 	return op
 }
 
-func montaMenu() {
+func showMenu() {
 	fmt.Println("")
 	fmt.Println("1 - Inicar Monitoramento")
 	fmt.Println("2 - Exibir Logs")
